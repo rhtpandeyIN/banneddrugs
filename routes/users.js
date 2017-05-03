55b5d2e5e0c9089ed87ed1e8556d8b043f153506
rhtpandeyIN <rhtpandey@in.ibm.com>
2017-05-04 00:09:54 +0530

    Rest api more dynamic

--- routes/users.js
+++ routes/users.js
@@ -10,22 +10,22 @@ cn="DRIVER={DB2 ODBC Driver};DATABASE=SAMPLE;HOSTNAME=localhost;UID=rohit;PWD=D#
 router.get('/druglist', function(req, res) {
     var db = req.db;
     db.open(cn, function (err,conn){
-      if(err){ res.json(err); return console.log(err);}
-    
-      conn.query("select * from drug WHERE drugName = 'PCM'", function(err, data) {
+      if(err){ res.json(err); console.log(err); conn.closeSync(); }
+      
+      else {
+        conn.query("select * from drug", function(err, data) {
 
-        if(err) {
-          res.json(err);
-          console.log("select error :" +err);
-        } else {
-          res.json(data);
-        }
+          if(err) {
+            res.json(err);
+            console.log("select error :" +err);
+          }else {
+            res.json(data);
+          }
 
-        conn.close(function() {
-          console.log("connection closed");
+          conn.close(function() { console.log("connection closed"); });
+
         });
-      });
-
+      }
     });
   });
 
@@ -33,40 +33,44 @@ router.get('/druglist', function(req, res) {
   /*
  * GET drug (search).
  */
+
 router.get('/search/:drugname', function(req, res) {
     var db = req.db;
     db.open(cn, function (err,conn){
-      if(err){ res.json(err); return console.log(err);}
+      if(err){ res.json(err); console.log(err); conn.closeSync(); }
+      
+      else {
+        var drug = req.params.drugname;
+        console.log("searched drug :" + drug);
 
-      var drug = req.params.drugname;
-      console.log("searched drug :" + drug);
+        conn.prepare("select * from drug WHERE drugName = ?", function (err, stmt) {
+          if (err) { res.json(err); console.log(err); conn.closeSync(); }
 
-      conn.prepare("select * from drug WHERE drugName = ?", function (err, stmt) {
-        if (err) { res.json(err); console.log(err); conn.closeSync(); }
-
-        stmt.execute([drug], function (err, result) {
-          if (err) { res.json(err); console.log(err); }
           else {
-            var data = result.fetchAllSync();
-            if(data.length !== 0) {
-              console.log(data);
-              res.json(data);
-            }else {
-              console.log("No such drug found");
-              res.json("No such drug found");
-            }  
-            result.closeSync();
-            stmt.closeSync();
+            stmt.execute([drug], function (err, result) {
+              if (err) { res.json(err); console.log(err); }
+              else {
+                var data = result.fetchAllSync();
+                if(data.length !== 0) {
+                  res.json(data);
+                }else {
+                  res.json("No such drug found");
+                  console.log("No such drug found");
+                }
+
+                result.closeSync();
+                stmt.closeSync();
+              }
+
+              conn.close(function() { console.log("connection closed"); });
+            });  
           }
 
-          conn.close(function() {
-          console.log("connection closed");
-          });
         });
-      });
-
+      }
     });
   });
+
 
 /*
  * POST to createdb.
@@ -74,45 +78,65 @@ router.get('/search/:drugname', function(req, res) {
 router.post('/createdb', function(req, res) {
     var db = req.db;
     db.open(cn, function (err,conn){
-      if(err){ res.json(err); return console.log(err);}
-    
-      conn.query("create table drug (drugID int, drugName char(30))", function(err) {
-    
-      if(err){
-        res.json(err);
-        console.log("db creation error :" +err);   
-      }else{
-        res.json("db created");
-        console.log("db created");
+      if(err){ res.json(err); console.log(err); conn.closeSync(); }
+      
+      else {
+        conn.query("create table drug (drugID int, drugName char(30))", function(err) {
+
+          if(err){
+            res.json(err);
+            console.log("db creation error :" +err);
+          }else {
+            res.json("db created");
+            console.log("db created");
+          }
+
+          conn.close(function(){console.log("connection closed");});
+
+        });
       }
-      conn.close(function(){console.log("connection closed");});
-      });
    });
 });
 
 
 /*
- * POST to inserting drug.
+ * POST to inserting drug (Single)
+ * send : drugid : "value", drugname : "value"
  */
 router.post('/addbanneddrug', function(req, res) {
+  var drugID = req.body.drugid;
+  var drugName = req.body.drugname;
   var db = req.db;
-  db.open(cn, function (err, conn){
-    if(err){ res.json(err); return console.log(err);}
 
-    conn.query("insert into drug values(1, 'PCM')", function(err) {
-      
-      if(err){
-        res.json(err);
-        console.log("db insert error :" +err);   
-      }else{
-        res.json("drug added");
-        console.log("drug added");
+  if(!drugID || !drugName) {
+    if(!drugID) res.json("Invaild data format- 'drugid'");
+    else if(!drugName) res.json("Invaild data format- 'drugname'");
+  }
+  else {
+    db.open(cn, function (err, conn) {
+      if(err){ res.json(err); console.log(err); conn.closeSync(); }
+
+      else {
+        conn.prepare("insert into drug (drugID, drugName) VALUES (?, ?)", function (err, stmt) {
+          if (err) { res.json(err); console.log(err); conn.closeSync(); }
+          
+          else {
+            stmt.execute([drugID, drugName], function (err, result) {
+              if(err) { res.json(err); console.log("db insert error :"+ err); }
+              else {
+                res.json("" + req.body.drugname +" added successfully");
+                result.closeSync();
+                stmt.closeSync();
+              }
+              
+              conn.close(function() { console.log("connection closed"); });   
+            });
+          }
+
+        });
       }
-
-      conn.close(function(){console.log("connection closed");});
     });
-
-  });
+  }
 });
 
 
@@ -122,35 +146,27 @@ router.post('/addbanneddrug', function(req, res) {
 router.post('/deletedb', function(req, res) {
   var db = req.db;
   db.open(cn, function (err, conn){
-    if(err){ res.json(err); return console.log(err);}
+    if(err){ res.json(err); console.log(err); conn.closeSync();}
+    
+    else {
+      conn.prepare("drop table drug", function(err, stmt) {
+        if(err) { res.json(err); console.log(err); conn.closeSync(); }
+        
+        else {
+          stmt.execute([], function(err, result) {
+            if(err) { res.json(err); console.log("delete execute error :"+err); }
+            else {
+              res.json("db dropped");
+              result.closeSync();
+              stmt.closeSync();
+            }
 
-    conn.prepare("drop table drug", function(err, stmt) {
-      
-      if(err) {
-        console.log("delete table error :"+err);
-        conn.close(function() { 
-          console.log("connection closed" );
-        });
-        res.json(err);
-      }else {
-        stmt.execute([], function(err, result) {
-          
-          if(err) {
-            res.json(err);
-            console.log("delete execute error :"+err);
-          }else {
-            result.closeSync();
-            res.json("db dropped");
-            console.log("db dropped");
-          }
-
-          conn.close(function() {
-            console.log("connection closed" );
+            conn.close(function() { console.log("connection closed"); });
           });
-        });
-      }
+        }
 
-    });
+      });
+    }
   }); 
 });
 
